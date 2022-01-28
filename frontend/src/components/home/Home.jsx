00873acf1bb6a5a3f0f42db436222e05cb233946
Edson Guilhem <edson_guilhem@hotmail.com>
2022-01-28 16:35:30 -0300

    Criando crud

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Main from "../template/Main";
+
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Segundo Titulo" >
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className='mb-0'>Sistema de Cadastro de Clientes TDR!</p>
+    </Main> 
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+            <span>
+                    Desenvolvido com <i className="fa fa-heart text-danger"></i> por  
+                    <strong> Edson <span className='text-danger'>Guilhem</span></strong>       
+            </span>
+    </footer>
