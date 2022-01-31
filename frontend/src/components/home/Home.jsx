faa6bb556d87f70c747a9fb6c8193c46d3fc4535
Edson Guilhem <edson_guilhem@hotmail.com>
2022-01-31 15:09:57 -0300

    Cadastrando Usuários

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -4,7 +4,7 @@ import Main from "../template/Main";
 
 export default props =>
     <Main icon="home" title="Início"
-        subtitle="Segundo Titulo" >
+        subtitle="Cadastro de Clientes" >
         <div className='display-4'>Bem Vindo!</div>
         <hr />
         <p className='mb-0'>Sistema de Cadastro de Clientes TDR!</p>
